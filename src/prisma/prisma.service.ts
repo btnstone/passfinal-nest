c9ba5401f51b4eee664e1970ff29a92ec14a5f70
Anya Fisher <139532176+btnstone@users.noreply.github.com>
2024-02-01 17:34:49 +0800

    feat: prisma开启调试

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,29 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: [
+        { level: 'query', emit: 'event' },
+        { level: 'error', emit: 'stdout' },
+        { level: 'info', emit: 'stdout' },
+        { level: 'warn', emit: 'stdout' },
+      ],
+    });
+
+    // 显式指定事件对象的类型
+    // @ts-ignore
+    this.$on('query', (e: Prisma.QueryEvent) => {
+      console.log(`Query: ${e.query}`);
+      console.log(`Duration: ${e.duration}ms`);
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
